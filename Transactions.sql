3c113a01269995329efd5df8d5c3040a35a016a3
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-02-14 12:55:43 -0500

    Update Transactions.sql

--- Transactions.sql
+++ Transactions.sql
@@ -1,5 +1,23 @@
 
 /*learning transactions*/
+/*PROPERTY OF TRANSACTION*/
+/*1. ATOMIC --- ROLLBACK UNDO THE CHANGES. 
+ Atomicity guarantees that each transaction is treated as a single "unit", which either succeeds completely, or fails completely:
+  if any of the statements constituting a transaction fails to complete, the entire transaction fails and the database is left unchanged. 
+  An atomic system must guarantee atomicity in each and every situation, including power failures, errors and crashes.
+2. CONSISTENT --  SHOULD BE LOGICAL. Consistency ensures that a transaction can only bring the database from one valid state to another,
+ maintaining database invariants: any data written to the database must be valid according to all defined rules, including constraints, 
+ cascades, triggers, and any combination thereof. This prevents database corruption by an illegal transaction, but does not guarantee 
+ that a transaction is correct.
+3. ISOLATED -- Transactions are often executed concurrently (e.g., reading and writing to multiple tables at the same time)
+ Isolation ensures that concurrent execution of transactions leaves the database in the same state that would have been
+ obtained if the transactions were executed sequentially. 
+4. DURABLE -- Durability guarantees that once a transaction has been committed, it will remain committed even in the case 
+of a system failure (e.g., power outage or crash).
+ This usually means that completed transactions (or their effects) are recorded in non-volatile memory. */
+
+
+
 select * from product_details
 select * from Product_Sales
 
